c746dd26b87c9345ffb18ec790829badc6376bf0
Alessandro Rocchello <alerocchello2001@gmail.com>
2025-08-22 10:13:54 +0200

    Fixing Playground

--- src/main/java/unibs/asd/playgrounds/Playground.java
+++ src/main/java/unibs/asd/playgrounds/Playground.java
@@ -1,6 +1,7 @@
 package unibs.asd.playgrounds;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import unibs.asd.enums.BitSetType;
 import unibs.asd.fileio.BenchmarkReader;
@@ -11,21 +12,24 @@ import unibs.asd.mhs.BoostMHS;
 
 public class Playground {
 
-    /*
-     * Il tempo limite deve essere espresso in secondi
-     */
-    public static final String BENCHMARK = "74L85.000.matrix";
-    public static final long TIMEOUT = 60;
-
     public static void main(String[] args) throws IOException {
-        boolean[][] benchmark = BenchmarkReader.readBenchmark("src\\mybenchmarks\\" + BENCHMARK);
-        MHS mhs;
-        mhs = new BoostMHS(benchmark);
-        mhs.run(BitSetType.FAST_BITSET, TIMEOUT * 1000);
-        System.out.println("Solutions found: " + mhs.getSolutions().size());
-        //System.out.println("Solutions: " + mhs.getSolutions());
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci il nome del file benchmark (es. 74L85.000.matrix): ");
+        String benchmark = scanner.nextLine();
+        
+        System.out.print("Inserisci il tempo massimo di esecuzione (in secondi): ");
+        long timeout = scanner.nextLong();
+
+        boolean[][] matrix = BenchmarkReader.readBenchmark("src\\mybenchmarks\\" + benchmark);
+        MHS mhs = new BoostMHS(matrix);
+        mhs.run(BitSetType.FAST_BITSET, timeout * 1000);
+
+        System.out.println("Soluzioni trovate: " + mhs.getSolutions().size());
         System.out.println("Tempo di esecuzione: " + mhs.getComputationTime()/1_000_000_000F + " secondi");
-        BenchmarkWriter.writeBenchmark(mhs, BENCHMARK, "results");
+        
+        BenchmarkWriter.writeBenchmark(mhs, benchmark, "results");
+        scanner.close();
     }
 
 }
